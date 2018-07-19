573a554a4647c288aa0c9e270bc175d094f69bf2
samer054 <sameralatawneh@gmail.com>
2018-07-19 19:48:15 +0300

    CSS training

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+
+
+
+//      document.getElementById("btnsettings").addEventListener("click",
+//                                                             function(){
+////              document.getElementsByClassName('submenu')[0].style.display = "block";
+
+ //      })
+       
+     
